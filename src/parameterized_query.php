79cfc36732d2661f902e505a3073b9883f3ef4de
Natthaphon <natthaphon.vi@kmitl.ac.th>
2019-11-01 11:44:42 +0700

    Add fucntion paraemterized query

---
+++ src/parameterized_query.php
@@ -0,0 +1,18 @@
+<?PHP
+    $dsn = "mysql:host=localhost;dbname=oture";
+    $username = "tony";
+    $password = "0m46emJj8p0ziPC6";
+
+    $pdo = new PDO($dsn, $username, $password);
+
+    $id = 12;
+    $stm = $pdo->prepare("SELECT * FROM countries WHERE ID = ?");
+    $stm->bindValue(1, $id);
+    $stm->execute();
+
+    $row = $stm->fetch(PDO::FETCH_ASSOC);
+
+    echo "id : " . $row['id'] . PHP_EOL;
+    echo "name : " . $row['name'] . PHP_EOL;
+    echo "Population" . $row['population'] . PHP_EOL;
+?>
