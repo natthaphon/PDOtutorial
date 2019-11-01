c458a2999bcbb561418762c845e95c2576fd20c5
Natthaphon <natthaphon.vi@kmitl.ac.th>
2019-11-01 11:29:53 +0700

    Add fucntion fetch_style_assoc

---
+++ src/fetch_style_assoc.php
@@ -0,0 +1,16 @@
+<?PHP
+    $dsn = "mysql:host=localhost;dbname=oture";
+    $username = "tony";
+    $password = "0m46emJj8p0ziPC6";
+
+    $pdo = new PDO($dsn, $username, $password);
+
+    $stm = $pdo->quey("SELECT * FROM countries");
+
+    $rows = $stm->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach($rows as $row){
+        print("{$row['id']} {$row['name']} {$row['population']}\n ");
+
+    }
+?>
