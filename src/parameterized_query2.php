394590092913cc0326fb98cb30788a4c48e24fb5
Natthaphon <natthaphon.vi@kmitl.ac.th>
2019-11-01 13:38:46 +0700

    Add fucntion paraemterized query2

---
+++ src/parameterized_query2.php
@@ -0,0 +1,20 @@
+<?PHP
+    $dsn = "mysql:host=localhost;dbname=oture";
+    $username = "oture";
+    $password = "0m46emJj8p0ziPC6";
+
+    $pdo = new PDO($dsn, $username, $password);
+
+    $id = 12;
+
+    $stm = $pdo->prepare("SELECT * FROM countries WHERE id = :id");
+    $stm->bindParam(":id", $id, PDO::PARAM_INT);
+    $stm->execute();
+
+    $row = $stm->fetch(PDO::FETCH_ASSOC);
+
+    echo "ID : " . $row['id'] . PHP_EOL;
+    echo "Name : " . $row['name'] . PHP_EOL;
+    echo "Populcatio : " . $row['population'] . PHP_EOL;
+    
+?>
