b7ff1ec516289a0f3935fe9c18d823bd74c0b872
Natthaphon <natthaphon.vi@kmitl.ac.th>
2019-11-01 15:39:37 +0700

    Add fucntion transaction

---
+++ src/transaction.php
@@ -0,0 +1,21 @@
+<?PHP
+    $dsn = "mysql:host=localhost;dbname=oture";
+    $username = "tony";
+    $password = "0m46emJj8p0ziPC6";
+
+    $pdo = new PDO($dsn, $username, $password);
+
+    try {
+
+        $pdo->beginTransaction();
+        $stm = $pdo->exec("INSERT INTO countries(name,population) VALUE ('Iraq', 3827400)");
+        $stm = $pdo->exec("INSERT INTO countries(name,population) VALUE ('Uganda', 37673800)");
+
+        $pdo->commit();
+    } catch(Exception $e) {
+
+        $pdo->rollback();
+        throw $e;
+    }
+
+?>
