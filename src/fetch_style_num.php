fc25efe7af9393924f498adbb4fe8808dd062d8a
Natthaphon <natthaphon.vi@kmitl.ac.th>
2019-11-01 10:14:33 +0700

    Add Fetch_style_num.php

---
+++ src/fetch_style_num.php
@@ -0,0 +1,14 @@
+<?PHP
+    $dsn = "mysql:host=localhost;dbname=oture";
+    $username = "tony";
+    $password = "0m46emJj8p0ziPC6";
+
+    $pdo = new PDO($dsn, $username, $password);
+    $stm = $pdo->query("SELECT * FROM countries");
+    $rows = $stm->fetchAll(PDO::FETCH_NUM);
+
+    foreach($rows as $row){
+        printf("$row[0] $row[1] $row[2] \n" );
+
+    }
+?>
