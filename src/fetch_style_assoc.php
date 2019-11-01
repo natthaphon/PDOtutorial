5b9c6813e8319da27bc6cf6dc973e95a72cc22f1
Natthaphon <natthaphon.vi@kmitl.ac.th>
2019-11-01 11:36:47 +0700

    Add fucntion fetch_style_assoc

--- src/fetch_style_assoc.php
+++ src/fetch_style_assoc.php
@@ -10,7 +10,7 @@
     $rows = $stm->fetchAll(PDO::FETCH_ASSOC);
 
     foreach($rows as $row){
-        printf("{$row['id']} {$row['name']} {$row['population']} "." \n");
+        printf("{$row['id']} {$row['name']} {$row['population']},\n");
 
     }
 ?>
