f0ef7624c3793c1419cf6bf9ea969896f6089627
Natthaphon <natthaphon.vi@kmitl.ac.th>
2019-11-11 10:15:04 +0700

    Add fucntion count column

---
+++ conifg/dbconect.php
@@ -0,0 +1,8 @@
+<?PHP
+$dsn = "mysql:host=localhost;dbname=oture";
+    $username = "tony";
+    $password = "0m46emJj8p0ziPC6";
+
+    $pdo = new PDO($dsn, $username, $password);
+
+?>
---
+++ src/column_count.php
@@ -0,0 +1,9 @@
+<?PHP
+include_once '../config/dbconnec.php';
+
+$stm = $pdo->query("SELECT name, population FROM countries WHERE id=1");
+
+$ncols = $stm->columnCount();
+
+echo "The Result set contains $ ncols columns\n"
+?>
