5bf9ff2b91a9ee4be4af17df5f338c596eae09e0
Natthaphon <natthaphon.vi@kmitl.ac.th>
2019-11-11 11:34:52 +0700

    Add fucntion count column

--- src/column_count.php
+++ src/column_count.php
@@ -5,5 +5,5 @@ $stm = $pdo->query("SELECT name, population FROM countries WHERE id=1");
 
 $ncols = $stm->columnCount();
 
-echo "The Result set contains $ ncols columns\n"
+echo "The Result set contains $ ncols columns\n";
 ?>
